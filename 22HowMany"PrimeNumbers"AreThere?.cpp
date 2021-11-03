43204de679c28ef839e8a43ace9fe3cf0e553c6e
ashwingudhayakumar <88505120+ashwingudhayakumar@users.noreply.github.com>
2021-11-03 14:13:27 +0530

    question 22

---
+++ 22HowMany"PrimeNumbers"AreThere?.cpp
@@ -0,0 +1,40 @@
+/*
+Question:
+
+
+How Many "Prime Numbers" Are There?
+Create a function that finds how many prime numbers there are, up to the given integer.
+
+Examples
+primeNumbers(10) ➞ 4
+// 2, 3, 5 and 7
+
+primeNumbers(20) ➞ 8
+// 2, 3, 5, 7, 11, 13, 17 and 19
+
+primeNumbers(30) ➞ 10
+// 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29
+Notes
+N/A
+
+
+Answer:
+
+*/
+bool isPrime(int n){
+	int count=0;
+	for(int i=2;i<=n/2;i++){
+		if(n%i==0)count++;
+	}
+	if(count>0)return false;
+	else return true;
+}
+int primeNumbers(int num) {
+	int count=0;
+	for(int i=2;i<=num;i++){
+		if(isPrime(i)){
+			count++;
+		}
+	}
+	return count;
+}

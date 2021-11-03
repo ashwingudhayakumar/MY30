2a47383fd315ea8af8d4e27add450f3ee6a601d5
ashwingudhayakumar <88505120+ashwingudhayakumar@users.noreply.github.com>
2021-11-03 15:06:30 +0530

    question 24

---
+++ 24SumOfMissingNumbers.cpp
@@ -0,0 +1,48 @@
+/*
+
+
+
+Question:
+
+
+
+Sum of Missing Numbers
+Create a function that returns the sum of missing numbers.
+
+Examples
+sumMissingNumbers([1, 3, 5, 7, 10]) ➞ 29
+// 2 + 4 + 6 + 8 + 9
+
+sumMissingNumbers([10, 7, 5, 3, 1]) ➞ 29
+
+sumMissingNumbers([10, 20, 30, 40, 50, 60]) ➞ 1575
+Notes
+The minimum and maximum value of the given array are the inclusive bounds of the numeric range to consider when searching for missing numbers.
+
+
+
+
+Answer:
+
+
+*/
+
+
+
+int sumMissingNumbers(std::vector<int> arr) {
+	std::sort(arr.begin(),arr.end());
+	int sum=0;
+	for(int i=arr[0];i<=arr[arr.size()-1];i++){
+		int flag=0;
+		for(int j=0;j<arr.size();j++){
+			if(i==arr[j]){
+				flag=1;
+				break;
+			}
+		}
+		if(!flag){
+			sum+=i;
+		}
+	}
+	return sum;
+}

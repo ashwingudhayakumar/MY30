c76abe0a8c4ed8045010a3ff3065fd61e93ce083
ashwingudhayakumar <88505120+ashwingudhayakumar@users.noreply.github.com>
2021-11-03 15:27:11 +0530

    question 25

---
+++ 25NextPrime.cpp
@@ -0,0 +1,40 @@
+/*
+Question:
+
+
+Next Prime
+Given an integer, create a function that returns the next prime. If the number is prime, return the number itself.
+
+Examples
+nextPrime(12) ➞ 13
+
+nextPrime(24) ➞ 29
+
+nextPrime(11) ➞ 11
+// 11 is a prime, so we return the number itself.
+Notes
+Note: 1 is not a prime.
+
+
+
+Answer:
+
+
+*/
+
+#include <math.h>
+bool isPrime(int n){
+	int count=0;
+	for(int i=2;i<=n;i++){
+		if(n%i==0)count++;
+	}
+	if(count>1)return false;
+	else return true;
+}
+int nextPrime(int num) {
+	if(num==1)return 2;
+	while(1){
+		if(isPrime(num))return num;
+		else num++;
+	}
+}

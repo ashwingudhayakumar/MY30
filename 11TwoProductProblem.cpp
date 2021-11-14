92a9a99a47f64a8f4dd79633e9ca87e9930b511a
ashwingudhayakumar <88505120+ashwingudhayakumar@users.noreply.github.com>
2021-11-14 21:18:06 +0530

    11th question added main fn

--- 11TwoProductProblem.cpp
+++ 11TwoProductProblem.cpp
@@ -26,6 +26,34 @@ The array can have multiple solutions, so return the first full match that you h
 
 ANSWER:*/
 
+/*QUESTION:
+
+
+
+
+
+Two Product Problem
+Create a function that takes an array arr and a number n and returns an array of two integers from arr whose product equals n.
+
+Examples
+twoProduct([1, 2, -1, 4, 5], 20) ➞ [4, 5]
+
+twoProduct([1, 2, 3, 4, 5], 10) ➞ [2, 5]
+
+twoProduct([100, 12, 4, 1, 2], 15) ➞ []
+Note:
+Try doing this with 0(N) time complexity.
+No duplicates.
+In the array, there can be multiple solutions so return the solution with the lowest sum of indexes of product pairs (i.e. N = 10, solutions = [[2, 5], [10, 1]], indexes = [[600, 3000], [800, 900]], return [10, 1]).
+The array can have multiple solutions, so return the first full match that you have found.
+
+
+
+
+
+
+ANSWER:*/
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -50,8 +78,9 @@ vector<int> twoProduct(std::vector<int> arr, int n) {
 int main(){
 	std::vector<int> arr;
 	int size;
+	 cout<<"enter size \n";
 	cin>>size;
-    cout<<"enter size ";
+   
 	int n;
 	cout<<"enter arr elements ";
 	for(int i=0;i<size;i++){
@@ -63,5 +92,6 @@ int main(){
     cin>>n;
 	std::vector<int> result=twoProduct(arr,n);
     if(result.size()==0)cout<<"[]";
-	for(int i=0;i<result.size();i++){cout<<result[i];}
+	for(int i=0;i<result.size();i++){cout<<result[i]<<" ";}
 }
+
